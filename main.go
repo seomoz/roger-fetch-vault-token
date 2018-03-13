702d81fb9ea5775913bb8e42b9efe1490af43883
Joshua Nett <joshua@moz.com>
2018-03-13 15:32:33 -0700

    env flag for wrapper scripts to set the token ROGER-2600

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
-// roger-gatekepper-mesos takes environment variables and returns JSON with an unwrapped Vault Token
+// roger-gk-mesos takes environment variables and returns JSON with an unwrapped Vault Token
 // to stdout for the vaultexec plugin to use
 package main
 
 import (
 	"fmt"
+	"flag"
 	"os"
 	"encoding/json"
 
@@ -17,16 +18,22 @@ type VaultExecConfig struct {
 }
 
 func main() {
+	setVaultEnv := flag.Bool("env", false, "set VAULT_TOKEN environment variable instead of JSON output")
+	flag.Parse()
 	token, err := gatekeeper.EnvRequestVaultToken()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: could not fetch token: %s\n", err)
 		os.Exit(1)
 	}
-	vec := VaultExecConfig{Token: token}
-	b, err := json.Marshal(vec)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: conversion to JSON failed: %s\n", err)
-		os.Exit(1)
+	if *setVaultEnv == true {
+		fmt.Printf(token)
+	} else {
+		vec := VaultExecConfig{Token: token}
+		b, err := json.Marshal(vec)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "ERROR: conversion to JSON failed: %s\n", err)
+			os.Exit(1)
+		}
+		os.Stdout.Write(b)
 	}
-	os.Stdout.Write(b)
 }
