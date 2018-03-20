bff44c358001f7d643c51d3de2c70cdccd7e2f50
David Overcash <funnylookinhat@gmail.com>
2018-03-20 09:40:41 -0700

    Running gofmt on source and using log.Fatal

--- main.go
+++ main.go
@@ -1,40 +1,43 @@
-// roger-fetch-vault-toke takes environment variables and returns an unwrapped Vault Token
+// roger-fetch-vault-token takes environment variables and returns an unwrapped Vault Token
 // in a JSON to stdout for the vaultexec plugin to use or to stdout directly for
 // wrapper scripts to use
 package main
 
 import (
-	"fmt"
-	"flag"
-	"os"
 	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
 
 	"github.com/channelmeter/vault-gatekeeper-mesos/gatekeeper"
 )
 
-type VaultExecConfig struct {
+type vaultExecConfig struct {
 	Address string `json:"address"`
-	Token	string `json:"token"`
-	Path	string `json:"path"`
+	Token   string `json:"token"`
+	Path    string `json:"path"`
 }
 
 func main() {
-	echoToken := flag.Bool("echo-token", false, "echos unwrapped Vault token to stdout for use by wrapper scripts")
+	echoToken := flag.Bool(
+		"echo-token",
+		false,
+		"echos unwrapped Vault token to stdout for use by wrapper scripts")
 	flag.Parse()
+
 	token, err := gatekeeper.EnvRequestVaultToken()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERROR: could not fetch token: %s\n", err)
-		os.Exit(1)
+		log.Fatalf("ERROR: could not fetch token: %s\n", err)
 	}
+
 	if *echoToken == true {
 		fmt.Printf(token)
 	} else {
-		vec := VaultExecConfig{Token: token}
+		vec := vaultExecConfig{Token: token}
 		b, err := json.Marshal(vec)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "ERROR: conversion to JSON failed: %s\n", err)
-			os.Exit(1)
+			log.Fatalf("ERROR: conversion to JSON failed: %s\n", err)
 		}
-		os.Stdout.Write(b)
+		fmt.Printf(string(b))
 	}
 }
